3a024d2272392f127dc7be7d1acbf2913443c224
Jesher Minelli <jesherdevsk8@gmail.com>
2025-03-24 21:45:48 -0300

    improve top scores

--- src/components/TopScorers.js
+++ src/components/TopScorers.js
@@ -2,10 +2,11 @@ const TopScorers = ({ scorers }) => {
   return (
     <div className="overflow-x-auto shadow-lg sm:rounded-lg bg-gray-200 dark:bg-gray-900 p-2">
       <table className="min-w-full text-sm text-left text-gray-800 dark:text-gray-300">
-        <thead className="text-xs text-gray-700 uppercase bg-gradient-to-r from-blue-800 to-indigo-800 dark:bg-blue-900 dark:text-indigo-300">
+        <thead className="text-xs uppercase bg-gradient-to-r from-blue-800 to-indigo-800 dark:from-blue-700 dark:to-indigo-800 dark:text-gray-300">
           <tr>
-            <th className="px-2 sm:px-4 py-2 text-left text-white">Jogador</th>
-            <th className="px-2 sm:px-4 py-2 text-right text-white">Gols</th>
+            <th scope="col" className="px-2 sm:px-4 py-2 text-left text-white">Jogador</th>
+            <th scope="col" className="px-2 sm:px-4 py-2 text-center text-white">Gols</th>
+            <th scope="col" className="px-2 sm:px-4 py-2 text-right text-white">Média</th>
           </tr>
         </thead>
         <tbody>
@@ -14,7 +15,8 @@ const TopScorers = ({ scorers }) => {
               <td className="px-2 sm:px-4 py-2 font-semibold text-left text-gray-800 dark:text-gray-300 sm:pl-6">
                 {index + 1} &nbsp; {scorer.name}
               </td>
-              <td className="px-2 sm:px-4 py-2 text-right text-gray-800 dark:text-gray-300">{scorer.goals}</td>
+              <td className="px-2 sm:px-4 py-2 text-center text-gray-800 dark:text-gray-300">{scorer.goals}</td>
+              <td className="px-2 sm:px-4 py-2 text-right text-gray-800 dark:text-gray-300">{scorer.average_goals_per_match}</td>
             </tr>
           ))}
         </tbody>
